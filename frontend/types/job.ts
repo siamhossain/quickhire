691c21cf5fd372f4d9eefd87de51150830ec68fe
siam <shiamhossain1995@gmail.com>
2026-03-04 16:38:01 +0600

    Create centralized Job type definition

---
+++ frontend/types/job.ts
@@ -0,0 +1,10 @@
+export type Job = {
+  _id: string;
+  title: string;
+  company: string;
+  location: string;
+  type: string;
+  category: string;
+  description: string;
+  createdAt?: string;
+};
