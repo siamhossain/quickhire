d7a02594ea8718c65b8391ab882e2dd3b9680467
siam <shiamhossain1995@gmail.com>
2026-03-04 17:23:34 +0600

    Update navbar layout component

--- frontend/components/layout/Navbar.tsx
+++ frontend/components/layout/Navbar.tsx
@@ -4,14 +4,27 @@ export default function Navbar() {
   return (
     <nav className="border-b bg-white">
       <div className="max-w-6xl mx-auto p-4 flex justify-between">
-        <Link href="/" className="font-bold text-xl">
-          QuickHire
-        </Link>
+        <div className="flex items-center">
+            <Link href="/" className="font-bold text-xl pr-10">
+                QuickHire
+            </Link>
 
-        <div className="space-x-6">
-          <Link href="/jobs">Jobs</Link>
-          <Link href="/admin">Post Job</Link>
+            <div className="space-x-6">
+                <Link href="/jobs">Find Jobs</Link>
+                <Link href="/admin">Browse Companies</Link>
+            </div>
         </div>
+        
+
+        <div className="flex items-center">
+          <Link href="/login" className="text-sm font-medium">
+            Login
+          </Link>
+          <Link href="/signup" className="ml-4 text-sm font-medium">
+            Sign Up
+          </Link>
+        </div>
+
       </div>
     </nav>
   );
