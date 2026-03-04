efed8df56bde402d0be5be0b4487ba3a046f02d9
siam <shiamhossain1995@gmail.com>
2026-03-04 17:32:24 +0600

    Add initial Hero section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,10 @@
+import Hero from "@/components/home/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="">
-      <h1>Siam</h1>
+      <Hero />
     </div>
   );
 }
---
+++ frontend/components/home/Hero.tsx
@@ -0,0 +1,12 @@
+export default function Hero () {
+    return (
+        <section className="text-center py-20 bg-white">
+            <h1 className="text-4xl font-bold mb-4">
+                Find Your Dream Job Today
+            </h1>
+            <p className="text-gray-600">
+                Browse top companies hiring now.
+            </p>
+        </section>
+    );
+}
