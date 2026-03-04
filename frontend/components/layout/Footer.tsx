f0e9507fa67e4427fd3b155fa257fdc013709aab
siam <shiamhossain1995@gmail.com>
2026-03-04 16:55:07 +0600

    Add footer layout component

---
+++ frontend/components/layout/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="border-t mt-20 p-6 text-center text-gray-500 text-sm">
+      © {new Date().getFullYear()} QuickHire
+    </footer>
+  );
+}
